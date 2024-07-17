cfb305bd81b06f9b146be80e4ce5734b9576344e
Shola Quadri <squadri@adaptavist.com>
2024-07-17 16:23:25 +0100

    adds the ability to create and edit movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,35 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    @movie.update(movies_params)
+
+    redirect_to movie_path(@movie)
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movies_params)
+
+    @movie.save
+
+    redirect_to movie_path(@movie)
+  end
+
+  private
+
+  def movies_params
+     # the :movie sym is derived from the key name in the request params
+    params.require(:movie).permit(:title, :description, :rating, :total_gross, :release_date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
     root "movies#index"
-    
-    get "movies" => "movies#index"
-    get "movies/:id" => "movies#show", as: "movie"
+
+    # get "movies" => "movies#index"
+    # get "movies/new" => "movies#new"
+    # get "movies/:id" => "movies#show", as: "movie"
+    # get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+    # patch "movies/:id" => "movies#update"
+    resources :movies
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
