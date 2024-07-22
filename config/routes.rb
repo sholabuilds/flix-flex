6bb9c585b3bf805ac75005491073808031952958
Shola Quadri <squadri@adaptavist.com>
2024-07-22 18:12:33 +0100

    implements resources to add reviews to movies

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
     # get "movies/:id" => "movies#show", as: "movie"
     # get "movies/:id/edit" => "movies#edit", as: "edit_movie"
     # patch "movies/:id" => "movies#update"
-    resources :movies
+    resources :movies do
+      resources :reviews
+    end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
